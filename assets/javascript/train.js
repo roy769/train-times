f4b5ca15db56e3cbe4db844f0feaec94fc62803d
roy769 <roy@mortgallery.com>
2017-11-02 12:26:03 -0700

    moment updated

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -1,17 +1,16 @@
 // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyChEe-dt9TYZhj3AJUYSqcw_jjOpglc1Co",
-    authDomain: "train-this.firebaseapp.com",
-    databaseURL: "https://train-this.firebaseio.com",
-    projectId: "train-this",
-    storageBucket: "",
-    messagingSenderId: "193895585409"
-  };
-  
-  firebase.initializeApp(config);
+var config = {
+  apiKey: "AIzaSyChEe-dt9TYZhj3AJUYSqcw_jjOpglc1Co",
+  authDomain: "train-this.firebaseapp.com",
+  databaseURL: "https://train-this.firebaseio.com",
+  projectId: "train-this",
+  storageBucket: "train-this.appspot.com",
+  messagingSenderId: "193895585409"
+};
+firebase.initializeApp(config);
 
   //database
-  var database= firebase.database();
+  var trainThis= firebase.database();
 
   //button
   $("#submit").on("click",function(event) {
@@ -20,65 +19,75 @@ event.preventDefault();
 
 //grab input
 
-var getUsr= $("#usr").val().trim();
-var getDest= $("#dest").val.trim();
-var getTrain= $("#train").val.trim();
-var getFreq= $("#freq").val().trim();
+var trainName = $("#train-name-input").val().trim();
+var destination = $("#destination-input").val.trim();
+var firstTrainUnix = $("#first-train-input").val.trim();
+var frequency = $("#frequency-input").val().trim();
 
 
 //create local files
 
-var trainData = {
-name: getUsr,
-Destination: getDest,
-Train:getTrain,
-Frequency: getFreq
+var newTrain = {
+name: trainName,
+Destination: destination,
+Train:firstTrainUnix,
+Frequency: frequency
 };
 
 //push dta not set data to database
 
-database.ref().push(getUsr);
+trainThis.ref().push(newTrain);
 
 //log new stuff
 
-console.log(getUsr.name);
-console.log(getTrain.Train);
-console.log(getDest.Destination);
-console.log(getFreq.Frequency);
+console.log(newTrain.name);
+console.log(newTrain.Destination);
+console.log(firstTrainUnix);
+console.log(newTrain.frequency);
 
 //alert
 alert("Train info Updated");
 
 //clear textboxes
 
-$("usr").val("");
-$("dest").val("");
-$("train").val("");
-$("freq").val("");
+$("#train-name-input").val("");
+$("#destination-input").val("");
+$("#first-train-input").val("");
+$("#frequency-input").val("");
 
+return false;
 });
 //firebase event for adding to database
 
-database.ref().on("child_added",function(childSnapshot, prevChildKey) {
+trainThis.ref().on("child_added",function(childSnapshot, prevChildKey) {
   console.log(childSnapshot.val());
 
 
 //store in a variable
 
-var getUsr= childSnapshot.val().name;
-var getDest= childSnapshot.val().Destination;
-var getFreq= childSnapshot.val().Frequency;
-var getTrain= childSnapshot.val().Train;
+var tname = childSnapshot.val().name;
+var tDestination = childSnapshot.val().destination;
+var tFrequency = childSnapshot.val().frequency;
+var tFirstTrain = childSnapshot.val().firsTrain;
 
-console.log(getUsr);
-console.log(getTrain);
-console.log(getDest);
-console.log(getFreq);
+//math
+var differenceTimes =  moment().diff(moment.unix(tFirstTrain), "minutes");
+var tRemainder = moment().diff(moment.unix(tFirstTrain), "minutes")%tFrequency;
+var tMinutes = tFrequency - tRemainder;
+//arrival
+var tArrival = moment().add(tMinutes, "m").format("hh:mm A");
 
-var makePretty= moment.unix(getTrain).format("HH:mm");
+var smallTime = moment().format("hh:mm A");
+var X = moment().format("X");
+
+console.log(tMinutes);
+console.log(tArrival);
+console.log(smallTime);
+console.log(X);
+
+
 
 //add train data
 
-$("#train-table > tbody").append("<tr><td>"+ getUsr + "<tr><td>" + getTrain + "<tr><td>" + getDest + "<tr><td>" + getFreq + "<tr><td>");
-
+$("#train-table > tbody").append("<tr><td>"+ tName + "<tr><td>" + tDestination + "<tr><td>" + tFrequency + "<tr><td>" + getFreq + "<tr><td>" + "<tr><td>" + tArrival + "<tr><td>" + tMinutes  "<tr><td>");
 });
